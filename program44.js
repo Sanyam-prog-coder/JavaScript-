ab73d65aac156d007eb77e174b33705a4ec0f0f6
Sanyam Bhupendrakumar Ravne <ravnesanyam@gmail.com>
2026-04-16 11:23:13 +0530

    Add files via upload

---
+++ program44.js
@@ -0,0 +1,43 @@
+function EvenFactorial(No)
+{
+    if(No < 0)
+    {
+        No = -No
+    }
+
+    let i = 0
+    let Fact = 1
+
+    for(i = 1; i <= No; i += 2)
+    {
+        Fact = Fact * i
+    }
+
+    return Fact
+}
+
+function main()
+{
+    const realine = require("readline")
+
+    const rl = realine.createInterface
+    ({
+        input: process.stdin,
+        output: process.stdout
+    })
+
+    rl.question
+    ("Enter Number: ",function(input)
+    {
+        let iValue = parseInt(input)
+        let iRet = 0
+
+        iRet = EvenFactorial(iValue)
+
+        console.log("Even Factorial of Number is : ",iRet)
+
+        rl.close()
+    })
+}
+
+main()
