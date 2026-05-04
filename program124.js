39255eef1cacc323872603e8bc25a49eda24dbe2
Sanyam Bhupendrakumar Ravne <ravnesanyam@gmail.com>
2026-05-04 12:43:27 +0530

    Add files via upload

---
+++ program124.js
@@ -0,0 +1,38 @@
+class Logic
+{
+    static Display(Arr, Value)
+    {
+        let i = 0
+        
+        for(i = 0; i < Value; i++)
+        {
+            if(Arr[i] % 3 == 0 && Arr[i] % 5 == 0)
+            {
+                console.log(Arr[i])
+            }
+        }
+    }
+}
+
+class program
+{
+    static main()
+    {
+        const prompt = require("prompt-sync")();
+
+        let Size = parseInt(prompt("Enter Number of Elements: "))
+
+        let element = []
+        let i = 0
+
+        for(i = 0; i < Size; i++)
+        {
+            let Num = parseInt(prompt(`Enter Element ${i + 1}: `))
+            element.push(Num)
+        }
+
+        Logic.Display(element, Size)
+    }
+}
+
+program.main()
