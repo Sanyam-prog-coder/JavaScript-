1d17ce35abc1068d5b3171b4b851d1a637cb48ee
Sanyam Bhupendrakumar Ravne <ravnesanyam@gmail.com>
2026-05-06 17:06:24 +0530

    Add files via upload

---
+++ program128.js
@@ -0,0 +1,38 @@
+class Logic
+{
+    static Patten(No)
+    {
+        let i = 0
+
+        for(i = 1; i <= No; i++)
+        {
+            process.stdout.write(i + "\t" + "#" + "\t")
+        }
+    }  
+}
+
+class program
+{
+    static main()
+    {
+        const readline = require("readline")
+
+        const rl = readline.createInterface
+        ({
+            input: process.stdin,
+            output: process.stdout
+        })
+
+        rl.question
+        ("Enter Number of Element: ",function(input)
+        {
+            let Value = parseInt(input)
+
+            Logic.Patten(Value)
+
+            rl.close()
+        })
+    }
+}
+
+program.main()
